cda3af01483317cf9d1f36e80bbd3345f77a9918
code723 <code723@gmail.com>
2015-03-29 12:02:37 +0800

    update

---
+++ mobile/src/main/java/edu/guet/jjhome/guetw5/ProfileActivity.java
@@ -0,0 +1,11 @@
+package edu.guet.jjhome.guetw5;
+
+import android.app.Activity;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by home on 3/29/15.
+ */
+public class ProfileActivity extends PreferenceActivity {
+
+}
